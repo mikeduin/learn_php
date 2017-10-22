c6075b30326e8d35c1521244d7d4a18601f28889
Mike Duin <michael.s.duin@gmail.com>
2017-10-21 23:10:11 -0700

    finish web programming training

---
+++ Web/assets/include.php
@@ -0,0 +1,11 @@
+<?php
+
+// This file will be included on every page on our web app
+// Things that should be included on every page can be added to this file
+
+// PHP has a predefined variable, $_SESSION, used in conjunction with session_start()
+// $_SESSION is an associative array, populated with any content you'd like to preserve from page
+// to page as a user navigates our web app
+
+session_start();
+
